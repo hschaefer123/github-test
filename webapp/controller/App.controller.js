2704a8ccd93c730e63a2a0115de9221df055a04a
Schäfer Holger <holger.schaefer@gmail.com>
2018-05-07 11:03:38 +0000

    initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("de.openui5.developer.github-test.controller.App", {
+
+	});
+});
